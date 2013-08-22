6bc5aa9481c81e329945284367147335dacd5c6d
Szuturon <ronnyszutu@gmail.com>
2013-08-22 16:03:45 -0400

    connection to database established

--- crm.rb
+++ crm.rb
@@ -1,5 +1,9 @@
-class CRM_App
-	def initialize
+require "contact"
+require "database"
+
+class CRM
+	def initialize(name)
+		@crm_name = name
 		run_crm
 	end
 
@@ -7,23 +11,23 @@ class CRM_App
 		while @menu_input != 6
 			display_menu
 			get_menu_input
-			menu_selection
+			call_option
 		end
 	end
 
 	def display_menu
 		clear_screen
+		puts "#{@crm_name}\'s CRM"
 		puts "Please select one of the following options: "
-		puts "   [1] Add Contact"
-		puts "   [2] Modify Contact"
-		puts "   [3] Delete Contact"
-		puts "   [4] Display All"
-		puts "   [5] Display Attribute"
-		puts "   [6] Exit"
-		puts "\n\n\n"
+		puts "   [1] Add a new contact"
+		puts "   [2] Modify an existing contact"
+		puts "   [3] Delete a contact"
+		puts "   [4] Display all the contacts"
+		puts "   [5] Display an attribute"
+		puts "   [6] Exit\n"
 	end
 
-	def menu_selection
+	def call_option
 		add_contact if @menu_input == 1
 		modify_contact if @menu_input == 2
 		delete_contact if @menu_input == 3
@@ -31,8 +35,16 @@ class CRM_App
 		display_attribute if @menu_input == 5
 	end
 
-	def add_contact
-	
+	def add_new_contact
+		print "Enter First Name: "
+		first_name=(gets.chomp)
+		print "Enter Last Name: "
+		last_name=(gets.chomp)
+		print "Enter Email Address: "
+		email=(gets.chomp)
+		print "Enter a Note: "
+		note=(gets.chomp)
+		contact = Contact.new(first_name, last_name, email, note)
 	end
 
 	def modify_contact
@@ -58,9 +70,13 @@ class CRM_App
 	def get_menu_input
 		@menu_input = 0
 		while (@menu_input < 1) or (@menu_input > 6)
+			print "Enter a number: "
 			@menu_input = gets.chomp.to_i
+			if (@menu_input < 1) or (@menu_input > 6)
+				puts "You've made an invalid selection. Try again."
+			end
 		end
 	end
 end
 
-CRM_App.new
+CRM.new("Ronny")
---
+++ database.rb
@@ -0,0 +1,14 @@
+class Database
+  @contacts = []
+  @id = 1000
+
+  def self.add_contact(contact)
+    contact.id = @id
+    @contacts << contact
+    @id += 1
+  end
+
+  def self.contacts
+    @contacts
+  end
+end
